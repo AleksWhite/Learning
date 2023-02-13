1b1b62688a974991a2eaa48ab9bfbad1aeaa807d
AlexWhite <TERRI123698745@yandex.ru>
2023-02-13 16:04:49 +0300

    update FileSystem

--- src/main/java/FileSystem/Main.java
+++ src/main/java/FileSystem/Main.java
@@ -1,7 +1,20 @@
 package FileSystem;
 
+import java.io.File;
+import java.io.FilenameFilter;
+import java.sql.SQLOutput;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        File f = new File("nbproject");
+        String[] l = f.list(new FilenameFilter() {
+            @Override
+            public boolean accept(File dir, String name) {
+                return name.startsWith("p");
+            }
+        });
+        for(String g : l){
+            System.out.println(g);
+        }
     }
 }
