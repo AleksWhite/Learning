3fc33e83f2b0b436a2b766d0d5cdd705597c00b3
AlexWhite <TERRI123698745@yandex.ru>
2023-04-30 10:23:36 +0300

    added FromStreamToMap

---
+++ src/main/java/Collectors/FromStreamToMap.java
@@ -0,0 +1,28 @@
+package Collectors;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+/*
+Из потока данных — в map
+*/
+
+public class FromStreamToMap {
+
+    public static void main(String[] args) {
+        var stringStream = Stream.of("JavaRush", "CodeGym", "Amigo", "Elly", "Kim", "Risha");
+
+        getMap(stringStream).forEach((s, i) -> System.out.println(s + " - " + i));
+    }
+
+    public static Map<String, Integer> getMap(Stream<String> stringStream) {
+        //напишите тут ваш код
+        Map<String, Integer> result = stringStream
+                .collect(Collectors.toMap(e -> e.toString(), e -> e.length()));
+        return result;
+        //короткий вариант
+        //return stringStream.collect(toMap(word -> word, String::length));
+    }
+}
