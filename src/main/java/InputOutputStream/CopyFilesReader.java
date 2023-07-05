aff90798e96a9f0bcd6b78be3c83a29496016b68
AlexWhite <TERRI123698745@yandex.ru>
2023-07-05 11:08:44 +0300

    added CopyFilesReader

---
+++ src/main/java/InputOutputStream/CopyFilesReader.java
@@ -0,0 +1,26 @@
+package InputOutputStream;
+
+
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class CopyFilesReader {
+    public static void main(String[] args) {
+        String src = "c:\\projects\\log.txt";
+        String dest = "c:\\projects\\copy.txt";
+
+        try(FileReader reader = new FileReader(src);
+            FileWriter writer = new FileWriter(dest))
+        {
+            char[] buffer = new char[65536]; // 64Kb
+            while (reader.ready())
+            {
+                int real = reader.read(buffer);
+                writer.write(buffer, 0, real);
+            }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
